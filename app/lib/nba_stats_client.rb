1ed1f8a4845aef633d761fc1e38e3736e9e1bd09
Todd Schneider <todd.w.schneider@gmail.com>
2019-11-11 19:34:49 -0500

    add referer header

--- app/lib/nba_stats_client.rb
+++ app/lib/nba_stats_client.rb
@@ -147,6 +147,7 @@ class NbaStatsClient
     connection: "keep-alive",
     host: "stats.nba.com",
     pragma: "no-cache",
+    referer: "https://www.nba.com/",
     upgrade_insecure_requests: "1",
     user_agent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9"
   }
